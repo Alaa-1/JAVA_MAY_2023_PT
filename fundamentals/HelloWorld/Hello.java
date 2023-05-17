1495eabaef231befec95daa5cab34c5e44ed5691
Alaa Abidi <aladdinabidi@gmail.com>
2023-05-17 14:08:43 +0100

    add hello world code

---
+++ fundamentals/HelloWorld/Hello.java
@@ -0,0 +1,20 @@
+public class Hello {
+// access modifier
+//    |
+//    |     return type
+//    |             |
+//    |             |   method name (class function)
+//    |             |     |   parameter
+//    V             V     V       v
+    public static void main(String[] args) {
+
+        // display "hello from java" in the console (terminal)
+        System.out.println("Hello from JAVA !"); 
+        
+	}
+}
+
+// to run this file you need first to compile it 
+// first by typing (javac Hello.java)
+// then type (java Hello) to execute it
+
