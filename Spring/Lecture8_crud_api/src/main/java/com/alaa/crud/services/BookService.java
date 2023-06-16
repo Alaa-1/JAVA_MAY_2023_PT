baf72ff55b2bc434ac33d4beb20b12f86e3d3183
Alaa Abidi <aladdinabidi@gmail.com>
2023-06-16 09:01:39 +0100

    #Lecture 8 - Spring MVC intro - APIController

---
+++ Spring/Lecture8_crud_api/src/main/java/com/alaa/crud/services/BookService.java
@@ -0,0 +1,47 @@
+package com.alaa.crud.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.alaa.crud.models.Book;
+import com.alaa.crud.repositories.BookRepository;
+
+@Service
+public class BookService {
+	
+    // adding the book repository as a dependency
+    private final BookRepository bookRepository;
+    
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    
+    
+//	==== CRUD FUNCTIONALITIES ===
+    
+    
+    
+    // returns all the books
+    // READ ALL
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    
+    // retrieves a book
+//    READ ONE
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+}
---
+++ Spring/Lecture8_crud_api/src/test/java/com/alaa/crud/Lecture7CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaa.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture7CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
